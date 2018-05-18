f55638968debf0fef8d6868e97fd7d2b83679070
Sundar Vasan <vr.sundar@gmail.com>
2018-05-18 14:58:12 -0700

    Remove spaces from the variable $winner
    This can otherwise cause problems where wc -l also includes spaces.
    
    Also quote it to be clear that it is a string comparion and make the
    check for draw be a function similary to winner() so the actual tests
    just invoke it like the winner tests.

--- test.sh
+++ test.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-
-for move in "rock" "paper" "scissors"; do
-    winner=`echo -e "$move\n$move\nexit" | ruby rock_paper_scissors.rb | grep draw |wc -l`
-    if ! [[ $winner == 1 ]]; then
-	echo "Both moves of $move did not finish in a draw"
+function draw() {
+    winner=`echo -e "$1\n$2\nexit" | ruby rock_paper_scissors.rb | grep draw |wc -l`
+    if ! [[ "${winner//[[:space:]]/}" == "1" ]]; then
+	echo "Both moves of $1 did not finish in a draw"
 	exit 1
     fi
-    echo "$move for both players is a draw"
-done
-
+    echo "$1 for both players is a draw"
+}
 
 function winner() {
     winner=`echo -e "$1\n$2\nexit" | ruby rock_paper_scissors.rb | grep "$3 wins" |wc -l`
-    if ! [[ $winner == 1 ]]; then
+    if ! [[ "${winner//[[:space:]]/}" == "1" ]]; then
 	echo "$3 did not win for $1 against $2"
 	exit 1
     fi
     echo "$3 wins for $1 against $2"
 
 }
+
+draw "rock" "rock"
+draw "paper" "paper"
+draw "scissors" "scissors"
 
 winner "rock" "scissors" 'Player1'
 winner "paper" "rock" 'Player1'
